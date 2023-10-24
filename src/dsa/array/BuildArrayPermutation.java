cd0c8abc29457da8600dae46361141025a68a68f
unknown <taniya.jobs.47@gmail.com>
2023-10-24 21:51:24 +0530

    added new code

---
+++ src/dsa/array/BuildArrayPermutation.java
@@ -0,0 +1,22 @@
+package dsa.array;
+
+import java.util.Arrays;
+
+public class BuildArrayPermutation {
+
+	public static void main(String[] args) {
+		int[] nums = {0,2,1,5,3,4};
+		System.out.println(Arrays.toString(buildArray(nums)));
+		
+	}
+
+	public static int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        
+        for (int i = 0; i < ans.length; i++) {
+			ans[i] = nums[nums[i]];
+		}
+        
+        return ans;
+    }
+}
